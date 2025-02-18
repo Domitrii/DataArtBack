67823fe20da519b6fd186fe3e3bdf560294fea78
Domitrii <126395589+Domitrii@users.noreply.github.com>
2025-02-18 01:45:42 +0000

    db change

--- db.js
+++ db.js
@@ -3,12 +3,18 @@ import mongoose from "mongoose"
 
 dotenv.config()
 
-const {DB_URI} = process.env
+const { DB_URI } = process.env
 
+if (!DB_URI) {
+  console.error("Error: DB_URI is not defined in .env");
+  process.exit(1);
+}
 
-mongoose.connect(DB_URI).then(console.log("db alright")).catch(error => {
-    console.error("u have error: ", error)
-    process.exit(1)
-}) 
-
-
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("db alright");
+  })
+  .catch(error => {
+    console.error("u have error: ", error);  
+    process.exit(1);  
+  });
